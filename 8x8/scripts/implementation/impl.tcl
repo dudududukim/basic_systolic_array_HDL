36a462786b17c366c819740fe07572f6ba2cdf45
Du-Hyeon <49020301+dudududukim@users.noreply.github.com>
2024-11-20 14:44:31 +0900

    impl tcl modified

--- 8x8/scripts/implementation/impl.tcl
+++ 8x8/scripts/implementation/impl.tcl
@@ -1,3 +1,3 @@
-open_project /home/dh/DUHYEON/basic_systolic_array/8x8/output/Created_Data/vec_mul_project_8x8/proj_1.xpr
+open_project /home/dh/DUHYEON/basic_systolic_array/8x8/output/Created_Data/systolic_project_8x8/proj_1.xpr
 
 launch_runs impl_1 -jobs 16
